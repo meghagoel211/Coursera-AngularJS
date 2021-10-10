079b2ddadf9e154237816b1cab9ce34a2f83dd13
Megha kickdrum <megha.goel@kickdrumtech.com>
2021-10-10 16:30:03 +0530

    changes

--- assignment_1/app.js
+++ assignment_1/app.js
@@ -7,18 +7,32 @@
     LunchController.$inject = ['$scope'];
     function LunchController($scope) {
         $scope.state="";
+        $scope.item="";
+        
         $scope.checkTooMuch = function () {
+
             var item = $scope.item;
+            
             var items = item.split(',');
-            if (items.length <4)
+            if($scope.item==""){
+                $scope.state="Please enter data first";
+            }
+            else{
+                if (items.length <4)
                 {
-                    console.log (items.length);
                     $scope.state = "Enjoy!";
                 }
-                
-        
+                    
+            
             else
                 $scope.state= "Too Much!";
+
+            }
+            
+           
+            
+
+          
             };
 
     }
